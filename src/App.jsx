812a2850179f8819b97c61fa80428a61a7f62d72
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-07-29 10:53:30 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import s from './App.module.css'
 
+import bannerPic from '/senaiBG.jpg'
+import card01 from '/pic01.jpg'
+import card02 from '/pic02.jpg'
+import card03 from '/pic03.jpg'
+import insta from '/instagram.png'
+import face from '/face.png'
+import git from '/github.png'
+
+
 function App() {
 
   return (
@@ -11,13 +20,12 @@ function App() {
       </nav>
       <main>
 
-        <section id='s1'>
-          <h1>TÍTULO DA EMPRESA</h1>
+        <section id='s1' className={s.s1}>
         </section>
 
         <section id='s2'>
           <div className={s.left}>
-            <img src="" alt="" />
+            <img width={300} src={bannerPic} alt="Banner" />
           </div>
           <div className={s.right}>
             <h2>Subtítulo</h2>
@@ -27,7 +35,7 @@ function App() {
 
         <section id='s3'>
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={300} src={card01} alt="" />
             <div className={s.cardTexts}>
               <h3>Título Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci amet fugiat quam placeat quisquam.</p>
@@ -35,7 +43,7 @@ function App() {
           </div>
 
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={300} src={card02} alt="" />
             <div className={s.cardTexts}>
               <h3>Título Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci amet fugiat quam placeat quisquam.</p>
@@ -43,7 +51,7 @@ function App() {
           </div>
 
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={300} src={card03} alt="" />
             <div className={s.cardTexts}>
               <h3>Título Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci amet fugiat quam placeat quisquam.</p>
@@ -52,9 +60,9 @@ function App() {
         </section>
       </main>
       <footer>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+        <a href=""><img width={40} src={insta} alt="" /></a>
+        <a href=""><img width={40} src={face} alt="" /></a>
+        <a href=""><img width={40} src={git} alt="" /></a>
       </footer>
     </>
   )
